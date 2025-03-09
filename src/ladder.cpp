3a2f91650f35c22bfeaedc6f9b607f18926eb2b1
vibha-y4 <vsyarlag@uci.edu>
2025-03-09 00:20:20 +0000

    verify_word_ladder

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -3,3 +3,18 @@
 void error(string word1, string word2, string msg) {
     cerr << "Error: " << msg << " (" << word1 << " and " << word2 << ")" << endl;
 }
+
+
+#define my_assert(e) {cout << #e << ((e) ? " passed": " failed") << endl;}
+
+void verify_word_ladder() {
+    set<string> word_list;
+    load_words(word_list, "words.txt");
+
+    my_assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
+    my_assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
+    my_assert(generate_word_ladder("code", "data", word_list).size() == 6);
+    my_assert(generate_word_ladder("work", "play", word_list).size() == 6);
+    my_assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
+    my_assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
+}
