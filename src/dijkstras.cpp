da70b1f4273557723453527420e607197cea0130
vibha-y4 <vsyarlag@uci.edu>
2025-03-09 02:04:49 +0000

    wording

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -50,7 +50,7 @@ void print_path(const vector<int>& v, int total){
     }
     for (size_t i = 0; i < v.size(); i++){
         cout << v[i];
-        if (i < v.size() - 1) cout << " -> ";
+        if (i < v.size() - 1) cout << " ";
     }
-    cout << ", Cost: " << total << endl;
+    cout << "Total cost is : " << total << endl;
 }
