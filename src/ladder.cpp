36590cb4b52bc8cc9b4092ca94b1624834c25bce
vibha-y4 <vsyarlag@uci.edu>
2025-03-09 00:30:18 +0000

    edit_distance_within for ladder

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -4,6 +4,28 @@ void error(string word1, string word2, string msg) {
     cerr << "Error: " << msg << " (" << word1 << " and " << word2 << ")" << endl;
 }
 
+bool edit_distance_within(const std::string& str1, const std::string& str2, int d){
+    if (abs((int)str1.length() - (int)str2.length()) > d){
+        return false;
+    }
+    
+    vector<vector<int>> dist(str1.length() + 1, vector<int>(str2.length() + 1));
+    for (int i = 0; i <= str1.length(); i++) dist[i][0] = i;
+    for (int j = 0; j <= str2.length(); j++) dist[0][j] = j;
+    
+    for (int i = 1; i <= str1.length(); i++){
+        for (int j = 1; j <= str2.length(); j++){
+            if (str1[i-1] == str2[j-1]) {
+                dist[i][j] = dist[i-1][j-1];
+            } 
+            else{
+                dist[i][j] = min({dist[i-1][j], dist[i][j-1], dist[i-1][j-1]}) + 1;
+            }
+        }
+    }
+    return dist[str1.length()][str2.length()] <= d;
+}
+
 
 #define my_assert(e) {cout << #e << ((e) ? " passed": " failed") << endl;}
 
