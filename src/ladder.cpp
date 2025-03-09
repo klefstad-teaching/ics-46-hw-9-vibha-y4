328fc430b7f74dc0f6f4f65f365de8b86101ce91
vibha-y4 <vsyarlag@uci.edu>
2025-03-09 21:40:11 +0000

    fixed error

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -1,46 +1,36 @@
 #include "ladder.h"
-#include <algorithm>
 
-void error(string word1, string word2, string msg) {
+void error(string word1, string word2, string msg){
     cerr << "Error: " << msg << " (" << word1 << " and " << word2 << ")" << endl;
 }
 
 bool edit_distance_within(const std::string& str1, const std::string& str2, int d){
-    int len1 = str1.length(), len2 = str2.length();
-    if (abs(len1 - len2) > 1) return false;
+    if (abs(str1.length() - str2.length()) > 1){
+        return false;
+    }
     
     int diff = 0, i = 0, j = 0;
-    while (i < len1 && j < len2) {
-        if (str1[i] != str2[j]) {
-            if (++diff > d) return false;
-            if (len1 > len2) ++i;
-            else if (len1 < len2) ++j;
-            else { ++i; ++j; }
-        } else {
-            ++i; ++j;
+    while (i < str1.length() && j < str2.length()){
+        if (str1[i] != str2[j]){
+            if (++diff > d){
+                return false;
+            }
+            if (str1.length() > str2.length()){
+                ++i;
+            }
+            else if (str1.length() < str2.length()){
+                ++j;
+            }
+            else{ 
+                ++i; ++j; 
+            }
+        } 
+        else{
+            ++i; 
+            ++j;
         }
     }
     return true;
-    // if (abs((int)str1.length() - (int)str2.length()) > d){
-    //     return false;
-    // }
-    
-    // vector<vector<size_t>> dist(str1.length() + 1, vector<size_t>(str2.length() + 1));
-    // for (size_t i = 0; i <= str1.length(); i++) dist[i][0] = i;
-    // for (size_t j = 0; j <= str2.length(); j++) dist[0][j] = j;
-    
-    // for (size_t i = 1; i <= str1.length(); i++){
-    //     for (size_t j = 1; j <= str2.length(); j++){
-    //         if (str1[i-1] == str2[j-1]) {
-    //             dist[i][j] = dist[i-1][j-1];
-    //         } 
-    //         else{
-    //             size_t min_val = std::min(dist[i-1][j], dist[i][j-1]);
-    //             dist[i][j] = std::min(min_val, dist[i-1][j-1]) + 1;
-    //         }
-    //     }
-    // }
-    // return dist[str1.length()][str2.length()] <= static_cast<size_t>(d);
 }
 
 bool is_adjacent(const string& word1, const string& word2){
@@ -91,7 +81,7 @@ void load_words(set<string>& word_list, const string& file_name){
     }
     string word;
     while (file >> word){
-        transform(word.begin(), word.end(), word.begin(), ::tolower);
+        //transform(word.begin(), word.end(), word.begin(), ::tolower);
         word_list.insert(word);
     }
     file.close();
